5a13350ba50decbb4cce88e5fc64b68b99151332
ishanbassi <ishanbassi@gmail.com>
2022-04-27 16:11:25 +0530

    added env table  argument for fullTmSearch function

--- .history/pages/api/fileReader_20220427160204.ts
+++ .history/pages/api/fileReader_20220427160204.ts
@@ -12,7 +12,6 @@ export default async function handler(req:NextApiRequest, res:NextApiResponse) {
         
         const tms:TmInterface[] = JSON.parse(req.body)
         const table = process.env.NODE_ENV == 'production' ? 'tm_details' : 'tm_detail'
-        
         const result =  await  fullTmSearch(tms, table)
         
         res.send(result)
