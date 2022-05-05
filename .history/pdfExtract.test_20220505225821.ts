5c60b8349581cbc0333af40bfd31b8fc9ef3d052
ishanbassi <ishanbassi@gmail.com>
2022-05-05 23:01:21 +0530

    modified pdf text extraction

--- .history/pdfExtract.test_20220505225821.ts
+++ .history/pdfExtract.test_20220505225821.ts
@@ -4,7 +4,7 @@ import { pdfIterator } from './seeds/02_more_data'
 jest.setTimeout(10000)
 import { extractPdfText } from './utilities/textExtraction'
 test('testing pdf extract' , async () => {
-    const result  = await extractPdfText('./pdfs/journal 2047/document8.pdf')
+    const result  = await extractPdfText('./pdfs/journal 2045/document4.pdf')
     await fsPromise.writeFile('pdf1.json', JSON.stringify(result), {encoding:'utf-8'})
     
 })
