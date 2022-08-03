b581a24cb7ab10c488729e772c997e1a42b6b5b1
ishanbassi <ishanbassi@gmail.com>
2022-08-03 19:10:40 +0530

    code splitting for image process

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import {Table, Container, Spinner, Button, Form, Row, Col} from 'react-bootstrap
 import {FileUploader} from 'react-drag-drop-files'
 import { CellObject, read, WorkSheet } from "xlsx"
 import { TmDataInterface } from "../utilities/textExtraction"
+import {createURL} from '../utilities/imageGenerator'
 interface TmSearchResInterface extends TmDataInterface{
     regtm :string
 }
@@ -21,34 +22,7 @@ const  App  = ({journals}) =>  {
     const tmClassArr = useRef([])
     const [journalNo,setJournalNo]   = useState<string>(journals[0].journal_no)
     
-    const createURL = (imgBuffer) => {
-        
-        
-            var mime;
-            var a = new Uint8Array(imgBuffer);
-            var nb = a.length;
-            if (nb < 4)
-                return null;
-            var b0 = a[0];
-            var b1 = a[1];
-            var b2 = a[2];
-            var b3 = a[3];
-            if (b0 == 0x89 && b1 == 0x50 && b2 == 0x4E && b3 == 0x47)
-                mime = 'image/png';
-            else if (b0 == 0xff && b1 == 0xd8)
-                mime = 'image/jpeg';
-            else if (b0 == 0x47 && b1 == 0x49 && b2 == 0x46)
-                mime = 'image/gif';
-            else
-                return null;
-            var binary = "";
-            for (var i = 0; i < nb; i++)
-                binary += String.fromCharCode(a[i]);
-            var base64 = window.btoa(binary);
-            
-            let imageSrc = 'data:' + mime + ';base64,' + base64;
-            return imageSrc
-    }
+    
     useEffect( () => {
         
         if(loading && tmClassArr.current.length > 0) {
