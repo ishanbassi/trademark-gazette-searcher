04e006a8440f12d082942e3a5433ff77c506eab7
ishanbassi <ishanbassi@gmail.com>
2022-08-16 13:20:34 +0530

    added error catching

--- utilities/imageCompressor.ts
+++ utilities/imageCompressor.ts
@@ -6,10 +6,17 @@ import imagemin from 'imagemin'
 
 export async function compressImg(imgBuffer:Buffer) {
     
-    let compressBuffer  = await imagemin.buffer(imgBuffer, {
+    let compressBuffer  = imagemin.buffer(imgBuffer, {
         plugins:[imageminJpegtran({
             progressive:true
         })]
+    })
+    .catch(reason =>{
+        if(reason.code == 2) {
+            console.log(reason)
+            return imgBuffer
+        }
+        
     })
     return compressBuffer
 }
